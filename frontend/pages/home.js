e6722d29d2581a66cc250813e8a2b90094e1de03
ThomasMoulins <13thomasmoulins@gmail.com>
2025-05-02 16:46:39 +0200

    Security when url change

--- frontend/pages/home.js
+++ frontend/pages/home.js
@@ -1,7 +1,17 @@
-import Home from '../components/Home';
+import Login from "../components/Login";
+import Home from "../components/Home";
+import { useSelector } from "react-redux";
+import { useRouter } from "next/router";
 
 function HomePage() {
- return <Home />;
+  const router = useRouter();
+  const user = useSelector((state) => state.user.value);
+
+  if (user.token) {
+    return <Home />;
+  } else {
+    router.push("/");
+  }
 }
 
 export default HomePage;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,17 @@
 import Login from "../components/Login";
+import Home from "../components/Home";
+import { useSelector } from "react-redux";
+import { useRouter } from "next/router";
 
 function Index() {
-  return <Login />;
+  const router = useRouter();
+  const user = useSelector((state) => state.user.value);
+
+  if (user.token) {
+    router.push("/home");
+  } else {
+    return <Login />;
+  }
 }
 
 export default Index;
