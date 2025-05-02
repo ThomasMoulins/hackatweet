0a75f60ac8745de638415b04518bd7927b7b5fff
Salim <salimdu13560@hotmail.com>
2025-05-02 12:39:24 +0200

    derniere route get

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -35,7 +35,7 @@ router.post('/signup', (req, res) => {
 })
 
 //connection
-router.get('/singin', (req, res) => {
+router.post('/signin', (req, res) => {
   if(!checkBody(req.body, ['username', 'password'])) {
     res.json({ result: false, error: 'Missing or empty fields' });
     return;
