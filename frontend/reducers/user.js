8b37461eb742cc9670548448e4738c2d8f297e6c
Salim <salimdu13560@hotmail.com>
2025-05-05 12:57:17 +0200

    liked fonctionnel

--- frontend/reducers/user.js
+++ frontend/reducers/user.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  value: { token: null, username: null, firstname: null },
+  value: { token: null, username: null, firstname: null, likedTweets: [] },
 };
 
 export const userSlice = createSlice({
@@ -12,14 +12,24 @@ export const userSlice = createSlice({
       state.value.token = action.payload.token;
       state.value.username = action.payload.username;
       state.value.firstname = action.payload.firstname;
+      state.value.likedTweets = action.payload.likedTweets;
     },
     logout: (state) => {
       state.value.token = null;
       state.value.username = null;
       state.value.firstname = null;
+      state.value.likedTweets = null;
     },
+    setLiked: (state, action) => {
+      if (state.value.likedTweets.some(e => e === action.payload.tweetId)) {
+        state.value.likedTweets = state.value.likedTweets.filter(e => e !== action.payload.tweetId)
+      } else {
+        state.value.likedTweets.push(action.payload.tweetId)
+      }
+      
+    }
   },
 });
 
-export const { login, logout } = userSlice.actions;
+export const { login, logout, setLiked } = userSlice.actions;
 export default userSlice.reducer;
